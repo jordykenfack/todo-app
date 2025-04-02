1310f6b253639f5d3e1f58bce7cf2ce8cf44b23d
jordykenfack <158819629+jordykenfack@users.noreply.github.com>
2025-04-02 20:17:54 +0300

    modify functions.py

--- functions.py
+++ functions.py
@@ -1,10 +1,15 @@
-def get_todos(filepath='todos.txt'):
+FILEPATH = 'todos.txt'
+
+def get_todos(filepath=FILEPATH):
     """ Read a text file and return the lst of to-do items.
     """
     with open(filepath , 'r') as file:
         todos_local = file.readlines()
     return todos_local
-def write_todos(todos_arg, filepath='todos.txt'):
+def write_todos(todos_arg, filepath=FILEPATH):
     """Write the to-do items list in the text file."""
     with open(filepath,'w') as file:
         file.writelines(todos_arg)
+
+if __name__ == '__main__':
+    get_todos()
